d764c0df63a2f5497e9bf80104884dfcb2a8a79e
Ali Ahmad <ali.neoworlder@proton.me>
2024-06-14 10:19:53 +0500

    added axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [pro, setPro] = useState([]);
+  const getData = async () => {
+    try {
+      const res = await axios.get("http://localhost:3000/products");
+      setPro(res.data.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    console.log("App is mounted");
+    getData();
+  }, []);
   return (
     <>
       {/* <ImageSection />
       <Header /> */}
+
+      {pro.map((ele) => (
+        <h1>{ele.name}</h1>
+      ))}
       <Counter />
 
       <p className='read-the-docs'>
@@ -20,10 +37,13 @@ function App() {
 const Counter = () => {
   const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+    console.log("Counter Updated: ", counter);
+  }, [counter]);
   return (
     <>
       <DisplayCounter counter={counter} />
-      <IncrementCounter setCounter={setCounter} />
+      <IncrementCounter setCounter={setCounter} counter={counter} />
     </>
   );
 };
@@ -32,9 +52,16 @@ const DisplayCounter = ({ counter }) => {
   return <>{counter}</>;
 };
 
-const IncrementCounter = ({ setCounter }) => {
+const IncrementCounter = ({ setCounter, counter }) => {
   return (
-    <button onClick={() => setCounter((previous) => previous + 1)}>Add</button>
+    <button
+      onClick={() => {
+        setCounter((previous) => previous + 1);
+        console.log("counter: ", counter);
+      }}
+    >
+      Add
+    </button>
   );
 };
 
